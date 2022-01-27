2a6e5b6713a666072d681bb121c7176a2f3bfd96
ZsofiErdodi <94741426+ZsofiErdodi@users.noreply.github.com>
2022-01-27 16:36:17 +0100

    Update utilTest.java

--- test/com/example/gui_basic/utilTest.java
+++ test/com/example/gui_basic/utilTest.java
@@ -21,19 +21,7 @@ class utilTest {
         util = new util();
     }
 
-    @AfterEach
-    void tearDown() {
-        //       util.clear();
-    }
-
-    @TestFactory
-    Stream<DynamicTest> dynamicTestsFromIntStream() {
-        // Generates tests for the first 10 even integers.
-        return IntStream.iterate(0, n -> n + 2).limit(10)
-                .mapToObj(n -> dynamicTest("test" + n, () -> assertTrue(n % 2 == 0)));
-    }
-
-
+    
     @DisplayName("Részeredmény és végeredmény számítását végző függvény tesztje; Paraméterezett teszt csv fájlból")
     @ParameterizedTest
     @CsvFileSource(resources = "/szamitasTeszt.csv", numLinesToSkip = 1)
